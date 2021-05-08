4dced9531cf2f61bd783cc352721fe3639129581
explne <ahmed@mufti.dev>
2021-05-08 19:14:40 +0200

    Added Density Plot and Histogram

--- exploratory.r
+++ exploratory.r
@@ -6,7 +6,7 @@ load_pac <- function(packages) {
  `%notin%` <- Negate(`%in%`)
   for (p in packages) {
     if (p %notin% rownames(installed.packages())) install.packages(p)
-    require(p , character.only = T)
+    if (p %notin% (.packages())) require(p , character.only = TRUE)
   }
 }
 
@@ -14,14 +14,58 @@ load_pac <- function(packages) {
 load_pac(packages)
 
 # load assignment data
-data <- read.csv("./Assignment.csv")
+data <- read_csv("./Assignment.csv")
 
-# number of claims during period of exposure
-nclaims <- data$nbrtotc
+
+
+spec <- list("AGEPH" = "age of the policy holder",
+             "duree" = "exposure (fraction of the year the insured is covered)",
+             "lnexpo" = "log of exposure",
+             "nbrtotc" = "total number of claims during period of exposure",
+             "nbrtotan" = "total number of claims during period of exposure/period of exposure",
+             "chargtot" = "total claim amount",
+             "agecar" = "age of the car: 0 − 1, 2 − 5, 6 − 10, > 10",
+             "sexp" = "sex of the policyholder: male or female",
+             "fuelc" = "type of fuel: petrol or gasoil",
+             "split" = "split of the premium: monthly, once, twice, three times per year",
+             "usec" = "use of the car: private or professional",
+             "fleetc" = "car belonging to a fleet: yes or no",
+             "sportc" = "sport car: yes or no",
+             "coverp" = "coverage: MTPL, MTPL+, MTPL+++",
+             "powerc" = "power of the car",
+             "CODPOSS" = "postal code in Belgium")
+
 
 # Percentage of total claims
-histogram(nclaims, type="percent", xlab="Number of Claims")
-per_tot <- table(nclaims)/length(nclaims) * 100
-print(per_tot)
+factor_hist <- function(factor, xlabel) {
+    covariate <- data[[factor]]
+    freq_table <- table(covariate)/length(covariate) * 100
+    print(freq_table)
+    hist <- histogram(covariate, type="percent", xlab= xlabel)
+    export <- str_interp("${factor}_hist.jpg")
+    if(!file.exists(export)) {
+    jpeg(export)
+    hist 
+    dev.off()
+    }
+    return(hist)
+}
+
+# Density plot 
+factor_dens <- function(factor, xlabel) {
+  covariate <- data[[factor]]
+  density <- plot(density(covariate), xlab = xlabel, main = "")
+  export <- str_interp("${factor}_density.jpg")
+  if(!file.exists(export)) {
+    jpeg(export)
+    density 
+    dev.off()
+  }
+  return(density)
+}
+
+factor_hist("nbrtotc", "Number of Claims")
+factor_dens("AGEPH", "Age of the policy holder")
+
 
 

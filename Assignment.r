529ddcd516f16058b7a1886b6e95674071177337
explne <ahmed@mufti.dev>
2021-05-15 21:56:32 +0200

    Added Spatial binning and plot

--- Assignment.r
+++ Assignment.r
@@ -52,7 +52,9 @@ data <- data %>%  rename_all(function(.name) {
 })
 
 # replace variable names
-data <- rename(data, c("expo" = "duree", "nclaims" = "nbrtotc", "claim_freq" = "nbrtotan"))
+data <- rename(data, c("expo" = "duree",
+                       "nclaims" = "nbrtotc",
+                       "claim_freq" = "nbrtotan"))
 
 # create tibble data frame
 df <- as_tibble(data)
@@ -79,7 +81,16 @@ struct <- list("coposs" = "postal code in Belgium",
 factors <- struct[9:length(struct)]
 
 # Select a reference group for the factors
-references <- list("2-5", "Female", "Petrol", "Once", "Private", "No", "No", "MTPL", "66-110")  
+references <- list("2-5", 
+                   "Female",
+                   "Petrol",
+                   "Once",
+                   "Private",
+                   "No",
+                   "No",
+                   "MTPL",
+                   "66-110")  
+
 names(references) <- names(factors)
 
 set_defaults <- function(references) {
@@ -106,34 +117,44 @@ y_label <- "Relative Frequency"
 # Bar plot
 plot_bar <- function(struct, save=F) {
   # convert names to symbols because aes() is a quoting function
-  # use syms() to tranform inputs into symbols that can be suitably unquoted
+  # use syms() to transform inputs into symbols that can be suitably unquoted
   cols <- syms(names(struct))
   for (col in cols) {
     # use tidy eval operator !! to unquote the symbol
     bar <- ggplot(data = df, aes(as.factor(!!col))) + 
-      theme_bw() + geom_bar(aes(y = after_stat(count/sum(count))), col = bg_color, fill = bg_color, alpha = 0.5) + 
-      labs(x = struct[[col]], y = y_label)
+                  theme_bw() + 
+                  geom_bar(aes(y = after_stat(count/sum(count))), 
+                  col = bg_color, fill = bg_color, alpha = 0.5) + 
+                  labs(x = struct[[col]], y = y_label)
+    # Print bar plot
     print(bar)
+    # Export if save is True/T
     if (save == T) {
-      export <- paste(as_string(col), "_bar.png")
+      export <- paste(as_string(col), "_bar.png", sep="")
       export_plot(export)
     }
   }
 }
 
 plot_hist <- function(coll, bin_width, save=F) {
+  # convert names to symbols because aes() is a quoting function
+  # use syms() to transform inputs into symbols that can be suitably unquoted
   cols <- syms(names(coll))
   for (col in cols) {
-    hist <-  ggplot(data = df, aes(!!col)) + theme_bw() + 
-      geom_histogram(aes(y = after_stat(count/sum(count))), binwidth = bin_width, col = bg_color, fill = bg_color, alpha = 0.5) + 
-      labs(x = coll[[col]], y = y_label)
+    # use tidy eval operator !! to unquote the symbol
+    hist <-  ggplot(data = df, aes(!!col)) +
+             theme_bw() + 
+             geom_histogram(aes(y = after_stat(count/sum(count))), 
+             binwidth = bin_width, col = bg_color, fill = bg_color, alpha = 0.5) + 
+             labs(x = coll[[col]], y = y_label)
+    # Print histogram
     print(hist)
+    # Export if save is True/T
     if (save == T) {
-      export <- paste(as_string(col), "_hist.png")
+      export <- paste(as_string(col),"_hist.png", sep="")
       export_plot(export)
     }
   }
-  
 }
 
 # frequency table
@@ -158,7 +179,7 @@ plot_dens <- function(coll, save=F) {
     density <- plot(density(covariate), xlab = coll[[n]], main = "")
     print(density)
     if (save == T) {
-      export <- paste(n,"_density.png")
+      export <- paste(n,"_density.png", sep="")
       export_plot(export)
     }
   }
@@ -180,6 +201,40 @@ plot_hist(struct[c("lnchargtot", "nclaims")], 1, T)
 # Frequency table for all categorical variables
 freq_table(factors)
 
+### Spatial data 
+belgium_shape_sf <- st_read('./shapefileBelgium/npc96_region_Project1.shp', quiet = TRUE)
+belgium_shape_sf <- st_transform(belgium_shape_sf, "+proj=longlat +datum=WGS84")
 
 
+## Total exposure observed over postal code
+post_expo <- df %>% group_by(codposs) %>% summarize(num = n(), total_expo = sum(expo)) 
+tot_post_expo <- post_expo %>% slice(1:5) %>% kable()
+print(tot_post_expo)
 
+## Merge Belgian shape file with constructed summary at postal code level
+belgium_shape_sf <- left_join(belgium_shape_sf, 
+                              post_expo, 
+                              by = c("POSTCODE" = "codposs"))
+
+
+# Compute relative exposure over unit area
+belgium_shape_sf$freq <- belgium_shape_sf$total_expo/belgium_shape_sf$Shape_Area
+
+# Transform exposure over unit area  to binned version
+belgium_shape_sf$freq_class <- cut(belgium_shape_sf$freq, 
+                                   breaks = quantile(belgium_shape_sf$freq, c(0,0.2,0.8,1), na.rm = TRUE),
+                                   right = FALSE, include.lowest = TRUE, 
+                                   labels = c("low", "average", "high"))
+
+
+# Create a plot with Belgium shape file
+belgium_shape_plot <- ggplot(belgium_shape_sf) +
+                geom_sf(aes(fill = freq_class), 
+                colour = "black", size = 0.1) +
+                ggtitle("Spatialized claim frequency") + 
+                labs(fill = "Relative\nExposure") +
+                scale_fill_brewer(palette = "Blues", 
+                na.value = "white") + 
+                theme_bw()
+                  
+print(belgium_shape_plot) 

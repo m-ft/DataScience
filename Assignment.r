b82291b4a8921a786eb7f89c23af01b9ea006c37
explne <ahmed@mufti.dev>
2021-05-15 19:39:38 +0200

    Added Frequency Tables

--- Assignment.r
+++ Assignment.r
@@ -4,16 +4,23 @@
 #  - Boonya Chivapong (r0767206)
 #  - Emmett Mufti (r0775432)
 
-# shutdown graphics
-# graphics.off()
-
 # set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # R packages
-packages <- c("tidyverse", "lattice", "mgcv", "caret", "rmarkdown", "readxl")
+packages <- c("tidyverse", 
+              "lattice",
+              "mgcv",
+              "caret",
+              "rmarkdown",
+              "readxl",
+              "rlang",
+              "knitr", 
+              "evtree",
+              "classInt",
+              "sf")
 
-# Install the previously uninstalled packages and load all packages
+# Install the previously uninstalled packages and load all unloaded packages
 load_pac <- function(packages) {
   `%notin%` <- Negate(`%in%`)
   for (p in packages) {
@@ -22,14 +29,18 @@ load_pac <- function(packages) {
   }
 }
 
-# load packages in the vector `packages`
+# load packages in the vector "packages"
 load_pac(packages)
+
+# load frequency table
+load("freq_tables.rda")
 
 # load assignment data
 claims <- read_csv("./Assignment.csv")
 postal <- read_excel("./inspost.xls")
 
 # merge data
+claims$lnchargtot <- log(claims$chargtot)
 data <- merge(claims, postal, by="CODPOSS")
 
 # tibble data frame
@@ -41,6 +52,7 @@ struct <- list("CODPOSS" = "postal code in Belgium",
                "nbrtotan" = "claim frequency rate",
                "duree" = "unit of exposure (fraction of year)",
                "chargtot" = "total claim amount",
+               "lnchargtot" = "log of total claim amount",
                "lnexpo" = "log of exposure",
                "agecar" = "age of the car",
                "sexp" = "sex of the policyholder",
@@ -53,10 +65,7 @@ struct <- list("CODPOSS" = "postal code in Belgium",
                "powerc" = "horsepower of the car")
 
 # Categorical variables
-factors <- struct[8:length(struct)]
-
-# Continuous variables
-continuous <- struct[2:5]
+factors <- struct[9:length(struct)]
 
 # Select a reference group for the factors
 references <- list("2-5", "Female", "Petrol", "Once", "Private", "No", "No", "MTPL", "66-110")  
@@ -78,68 +87,87 @@ bg_color <- "#009CFF"
 
 # Export plot to png file
 export_plot <- function(filename){
-  if(!file.exists(filename)) {
-    dev.print(png, filename = filename, width = 360, height= 360)
-  }
+  dev.print(png, filename = filename, width = 360, height= 360)
 }
 
 y_label <- "Relative Frequency"
 
 # Bar plot
-plot_bar <- function(struct) {
+plot_bar <- function(struct, save=F) {
   # convert names to symbols because aes() is a quoting function
   # use syms() to tranform inputs into symbols that can be suitably unquoted
   cols <- syms(names(struct))
   for (col in cols) {
     # use tidy eval operator !! to unquote the symbol
     bar <- ggplot(data = df, aes(as.factor(!!col))) + 
-      theme_bw() + geom_bar(aes(y = (..count..)/sum(..count..)), col = bg_color, fill = bg_color, alpha = 0.5) + 
+      theme_bw() + geom_bar(aes(y = after_stat(count/sum(count))), col = bg_color, fill = bg_color, alpha = 0.5) + 
       labs(x = struct[[col]], y = y_label)
     print(bar)
-    export <- paste(as_string(col), "_bar.png")
-    export_plot(export)
+    if (save == T) {
+      export <- paste(as_string(col), "_bar.png")
+      export_plot(export)
+    }
   }
 }
 
-plot_hist <- function(struct) {
-  cols <- syms(names(struct))
+plot_hist <- function(coll, bin_width, save=F) {
+  cols <- syms(names(coll))
   for (col in cols) {
-   hist <-  ggplot(data = df, aes(!!col)) + theme_bw() + 
-    geom_histogram(aes(y = (..count..)/sum(..count..)), binwidth = 1, col = bg_color, fill = bg_color, alpha = 0.5) + 
-    labs(x = struct[[col]], y = y_label)
+    hist <-  ggplot(data = df, aes(!!col)) + theme_bw() + 
+      geom_histogram(aes(y = after_stat(count/sum(count))), binwidth = bin_width, col = bg_color, fill = bg_color, alpha = 0.5) + 
+      labs(x = coll[[col]], y = y_label)
     print(hist)
-    export <- paste(as_string(col), "_hist.png")
-    export_plot(export)
+    if (save == T) {
+      export <- paste(as_string(col), "_hist.png")
+      export_plot(export)
+    }
   }
   
 }
 
 # frequency table
-freq_table <- function(struct) {
-  for (n in names(struct)) {
+freq_table <- function(coll) {
+  freq_tables <- c()
+  for (n in names(coll)) {
     covariate <- df[[n]]
     t <- as.data.frame(table(covariate)/length(covariate) * 100)
-    names(t)[1] = struct[[n]]
+    names(t)[1] =  coll[[n]]
     names(t)[2] = "Rel Freq"
-    print(t)  
+    k <- kable(t)
+    print(k)
+    freq_tables[[n]] <- k
   }
-  
+  save(freq_tables, file="freq_tables.rda")
 }
 
 # Density plot 
-plot_dens <- function(struct) {
-  for (n in names(struct)) {
+plot_dens <- function(coll, save=F) {
+  for (n in names(coll)) {
     covariate <- df[[n]]
-    density <- plot(density(covariate), xlab = struct[[n]], main = "")
+    density <- plot(density(covariate), xlab = coll[[n]], main = "")
     print(density)
-    export <- paste(n,"_density.png")
-    export_plot(export)
+    if (save == T) {
+      export <- paste(n,"_density.png")
+      export_plot(export)
+    }
   }
   
 }
 
-plot_bar(factors)
+## Plots and frequency tables 
+## use "T" or "TRUE" as the last argument to export plots
+
+# Bar plot for all categorical variables
+plot_bar(factors, T)
+# Frequency table for all categorical variables
 freq_table(factors)
-plot_hist(continuous)
+
+# Histogram number of claims during period of exposure 
+plot_hist(struct["AGEPH"], 0.5, T)
+
+# Histogram log of total claim amount and number of claims 
+plot_hist(struct[c("lnchargtot", "nbrtotc")], 1)
+
+
 
 
